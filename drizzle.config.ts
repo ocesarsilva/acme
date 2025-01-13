202721584aec496599f6a509c7ccd14b5917b756
Cesar Silva <cesarsilva.dev@gmail.com>
2025-01-12 21:38:18 -0300

    feat: add drizzle setup

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+import { env } from "@/env.js";
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./src/db/schema/index.ts",
+  dialect: "postgresql",
+  out: "./drizzle",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+} satisfies Config;
