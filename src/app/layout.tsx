7e79f5c26ef994d0b14031736673818e04652c97
Cesar Silva <cesarsilva.dev@gmail.com>
2025-01-12 21:17:17 -0300

    feat: add biomejs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import type { Metadata } from "next";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { geistMono, geistSans } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "min-h-svh bg-background font-sans antialiased",
+          geistSans.variable,
+          geistMono.variable,
+        )}
       >
+        <head />
         {children}
       </body>
     </html>
